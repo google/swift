8207b16db6341f9166744071752b1f6243b1a735
andrestamez <andrestamez@google.com>
2018-06-13 16:52:06 -0700

    Initial implementation of BinaryOperatorWhitespace

--- tools/swift-format/Sources/Rules/OperatorWhitespace.swift
+++ tools/swift-format/Sources/Rules/OperatorWhitespace.swift
@@ -1,15 +1,60 @@
-import Core
-import Foundation
-import SwiftSyntax
-
-/// Exactly one space must appear before and after each binary operator token.
-///
-/// Lint: If an invalid number of spaces appear before or after a binary operator, a lint error is
-///       raised.
-///
-/// Format: All binary operators will have a single space before and after.
-///
-/// - SeeAlso: https://google.github.io/swift#horizontal-whitespace
-public final class OperatorWhitespace: SyntaxFormatRule {
-
-}
+ import Core
+ import Foundation
+ import SwiftSyntax
+ 
+ /// Exactly one space must appear before and after each binary operator token.
+ ///
+ /// Lint: If an invalid number of spaces appear before or after a binary operator, a lint error is
+ ///       raised.
+ ///
+ /// Format: All binary operators will have a single space before and after.
+ ///
+ /// - SeeAlso: https://google.github.io/swift#horizontal-whitespace
+ public final class OperatorWhitespace: SyntaxFormatRule {
+  public override func visit(_ token: TokenSyntax) -> Syntax {
+    guard let nextToken = token.nextToken else { return token }
+    let exceptions =  ["...", "..<", ">.."]
+    
+    // Operators own their trailing spaces, so ensure it only has 1 space
+    // if there's another token in the same line.
+    if (token.tokenKind == .unspacedBinaryOperator(token.text) ||
+      token.tokenKind == .spacedBinaryOperator(token.text)),
+      !nextToken.leadingTrivia.containsNewlines,
+      !exceptions.contains(token.text){
+      return placeOneTrailingSpace(token: token)
+    }
+    
+    // Tokens before the operator should have a single space after.
+    if (nextToken.tokenKind == .unspacedBinaryOperator(nextToken.text) ||
+      nextToken.tokenKind == .spacedBinaryOperator(nextToken.text)),
+      !token.trailingTrivia.containsNewlines,
+      !exceptions.contains(nextToken.text) {
+      return placeOneTrailingSpace(token: token)
+    }
+    return token
+  }
+  
+  /// Returns a token with a single space in it's leading trivia, and raise
+  /// a lint error indicating if the operator needs a space before or after it.
+  private func placeOneTrailingSpace(token: TokenSyntax) -> TokenSyntax {
+    let numSpaces = token.trailingTrivia.numberOfSpaces
+    if numSpaces > 1 {
+      diagnose(.removesSpacesAfterOperator(count: numSpaces - 1, binOperator: token.text), on: token)
+    }
+    else if numSpaces == 0 {
+      diagnose(.addSpaceAfterOperator(binOperator: token.text), on: token)
+    }
+    return token.withOneTrailingSpace()
+  }
+ }
+ 
+ extension Diagnostic.Message {
+  static func removesSpacesAfterOperator(count: Int, binOperator: String) -> Diagnostic.Message {
+    let ending = count == 1 ? "" : "s"
+    return Diagnostic.Message(.warning, "remove \(count) space\(ending) after '\(binOperator)'")
+  }
+  
+  static func addSpaceAfterOperator(binOperator: String) -> Diagnostic.Message {
+    return Diagnostic.Message(.warning, "add one space after '\(binOperator)'")
+  }
+ }
---
+++ tools/swift-format/Tests/SwiftFormatTests/OperatorWhitspaceTests.swift
@@ -0,0 +1,41 @@
+import SwiftSyntax
+import XCTest
+
+@testable import Rules
+
+public class OperatorWhitespaceTests: DiagnosingTestCase {
+  public func testInvalidOperatorWhitespace() {
+    XCTAssertFormatting(
+      OperatorWhitespace.self,
+      input: """
+             for number in 1...5 {}
+             var a = -10+3
+             a*=2
+             let b: UInt8 = 4
+             b       << 1
+             b>>=2
+             let c: UInt8 = 0b00001111
+             let d = ~c
+             struct AnyEquatable<Wrapped : Equatable> : Equatable {}
+             func foo(param: x&y) {}
+             """,
+      expected: """
+                for number in 1...5 {}
+                var a = -10 + 3
+                a *= 2
+                let b: UInt8 = 4
+                b << 1
+                b >>= 2
+                let c: UInt8 = 0b00001111
+                let d = ~c
+                struct AnyEquatable<Wrapped : Equatable> : Equatable {}
+                func foo(param: x & y) {}
+                """)
+  }
+  
+  #if !os(macOS)
+  static let allTests = [
+    OperatorWhitespaceTests.testInvalidOperatorWhitespace,
+    ]
+  #endif
+}
