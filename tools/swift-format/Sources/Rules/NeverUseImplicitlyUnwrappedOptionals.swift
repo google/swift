2dea2d633d4991f3e20db5fc8b16a4972457dbcb
laurenwhite <laurenwhite@google.com>
2018-06-14 15:42:33 -0700

    Implement NeverUseImplicitlyUnwrappedOptionals rule and tests.

--- tools/swift-format/Sources/Rules/NeverUseImplicitlyUnwrappedOptionals.swift
+++ tools/swift-format/Sources/Rules/NeverUseImplicitlyUnwrappedOptionals.swift
@@ -6,13 +6,51 @@ import SwiftSyntax
 ///
 /// Certain properties (e.g. `@IBOutlet`) tied to the UI lifecycle are ignored.
 ///
-/// This rule does not apply to test code, defined as code which matches one or more of:
-///   * Parent directory named "Tests"
+/// This rule does not apply to test code, defined as code which:
 ///   * Contains the line `import XCTest`
+///
+/// TODO: Create exceptions for other UI elements (ex: viewDidLoad)
 ///
 /// Lint: Declaring a property with an implicitly unwrapped type yields a lint error.
 ///
 /// - SeeAlso: https://google.github.io/swift#implicitly-unwrapped-optionals
 public final class NeverUseImplicitlyUnwrappedOptionals: SyntaxLintRule {
+  
+  // Checks if "XCTest" is an import statement
+  public override func visit(_ node: ImportDeclSyntax) {
+    var iterator = node.path.makeIterator()
+    while let component = iterator.next() {
+      if component.name.text == "XCTest" {
+        context.importsXCTest = true
+      }
+    }
+  }
 
+  public override func visit(_ node: VariableDeclSyntax) {
+    if !context.importsXCTest {
+      // Ignores IBOutlet variables
+      if let attributes = node.attributes {
+        var iterator = attributes.makeIterator()
+        while let item = iterator.next() {
+          if item.attributeName.text == "IBOutlet" { return }
+        }
+      }
+      // Finds type annotation for variable(s)
+      for binding in node.bindings {
+        guard let nodeTypeAnnotation = binding.typeAnnotation else { continue }
+        diagnoseImplicitWrapViolation(nodeTypeAnnotation.type)
+      }
+    }
+  }
+
+  func diagnoseImplicitWrapViolation(_ type: TypeSyntax) {
+    guard let violation = type as? ImplicitlyUnwrappedOptionalTypeSyntax else { return }
+    diagnose(.doNotUseImplicitUnwrapping(identifier: "\(violation.wrappedType)"), on: type)
+  }
+}
+
+extension Diagnostic.Message {
+  static func doNotUseImplicitUnwrapping(identifier: String) -> Diagnostic.Message {
+    return .init(.warning, "Use \(identifier) or \(identifier)? instead of \(identifier)!")
+  }
 }
