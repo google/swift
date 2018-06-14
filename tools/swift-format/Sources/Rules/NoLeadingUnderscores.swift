d9dec65b3d4189643aa74451ff941a90583af44c
laurenwhite <laurenwhite@google.com>
2018-06-14 15:19:01 -0700

    Implement NoLeadingUnderscores rule and tests.

--- tools/swift-format/Sources/Rules/NoLeadingUnderscores.swift
+++ tools/swift-format/Sources/Rules/NoLeadingUnderscores.swift
@@ -11,5 +11,104 @@ import SwiftSyntax
 ///
 /// - SeeAlso: https://google.github.io/swift#naming-conventions-are-not-access-control
 public final class NoLeadingUnderscores: SyntaxLintRule {
+  
+  public override func visit(_ node: AssociatedtypeDeclSyntax) {
+    diagnoseUnderscoreViolation(name: node.identifier)
+  }
+  
+  public override func visit(_ node: ClassDeclSyntax) {
+    diagnoseUnderscoreViolation(name: node.identifier)
+    super.visit(node) // Visit children despite override
+  }
+  
+  public override func visit(_ node: EnumDeclSyntax) {
+    diagnoseUnderscoreViolation(name: node.identifier)
+    super.visit(node)
+  }
+  
+  public override func visit(_ node: EnumCaseDeclSyntax) {
+    var iterator = node.elements.makeIterator()
+    while let item = iterator.next() {
+      diagnoseUnderscoreViolation(name: item.identifier)
+    }
+  }
+  
+  public override func visit(_ node: FunctionDeclSyntax) {
+    diagnoseUnderscoreViolation(name: node.identifier)
+    // Check parameter names of function
+    var paramIterator = node.signature.input.parameterList.makeIterator()
+    while let parameter = paramIterator.next() {
+      if let typeIdentifier = parameter.firstName {
+        diagnoseUnderscoreViolation(name: typeIdentifier)
+      }
+      if let varIdentifier = parameter.secondName {
+        diagnoseUnderscoreViolation(name: varIdentifier)
+      }
+    }
+    // Check generic parameter names
+    var genParamIterator = node.genericParameterClause?.genericParameterList.makeIterator()
+    while let genParamter = genParamIterator?.next() {
+      diagnoseUnderscoreViolation(name: genParamter.name)
+    }
+    super.visit(node)
+  }
+  
+  public override func visit(_ node: PrecedenceGroupDeclSyntax) {
+    diagnoseUnderscoreViolation(name: node.identifier)
+  }
+  
+  public override func visit(_ node: ProtocolDeclSyntax) {
+    diagnoseUnderscoreViolation(name: node.identifier)
+    super.visit(node)
+  }
+  
+  public override func visit(_ node: StructDeclSyntax) {
+    diagnoseUnderscoreViolation(name: node.identifier)
+    // Check generic parameter names
+    var iterator = node.genericParameterClause?.genericParameterList.makeIterator()
+    while let genParam = iterator?.next() {
+        diagnoseUnderscoreViolation(name: genParam.name)
+    }
+    super.visit(node)
+  }
+  
+  public override func visit(_ node: TypealiasDeclSyntax) {
+    diagnoseUnderscoreViolation(name: node.identifier)
+  }
+  
+  public override func visit(_ node: InitializerDeclSyntax) {
+    // Check parameter names of initializer
+    var iterator = node.parameters.parameterList.makeIterator()
+    while let parameter = iterator.next() {
+      if let typeIdentifier = parameter.firstName {
+        diagnoseUnderscoreViolation(name: typeIdentifier)
+      }
+      if let varIdentifier = parameter.secondName {
+        diagnoseUnderscoreViolation(name: varIdentifier)
+      }
+    }
+    super.visit(node)
+  }
+  
+  public override func visit(_ node: VariableDeclSyntax) {
+    for binding in node.bindings {
+      if let pat = binding.pattern as? IdentifierPatternSyntax {
+        diagnoseUnderscoreViolation(name: pat.identifier)
+      }
+    }
+    super.visit(node)
+  }
+  
+  func diagnoseUnderscoreViolation(name: TokenSyntax) {
+    let leadingChar = name.text.first
+    if leadingChar == "_" {
+      diagnose(.doNotLeadWithUnderscore(identifier: name.text), on: name)
+    }
+  }
+}
 
+extension Diagnostic.Message {
+  static func doNotLeadWithUnderscore(identifier: String) -> Diagnostic.Message {
+    return .init(.warning, "Identifier \(identifier) should not lead with '_'")
+  }
 }
