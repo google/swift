3c2ef1d31ce8d03ddafa15520f04fadeedd7c205
laurenwhite <laurenwhite@google.com>
2018-06-14 15:32:52 -0700

    Implement NeverUseForceTry rule and tests.

--- tools/swift-format/Sources/Rules/NeverUseForceTry.swift
+++ tools/swift-format/Sources/Rules/NeverUseForceTry.swift
@@ -4,16 +4,36 @@ import SwiftSyntax
 
 /// Force-try (`try!`) is forbidden.
 ///
-/// This rule does not apply to test code, defined as code which matches one or more of:
-///   * Parent directory named "Tests"
+/// This rule does not apply to test code, defined as code which:
 ///   * Contains the line `import XCTest`
 ///
 /// Lint: Using `try!` results in a lint error.
 ///
-/// Format: The use of `try!` is replaced with a `try`...`catch` block where the `catch` block
-///         contains `fatalError("TODO(<username>): document before submitting")`
+/// TODO: Create exception for NSRegularExpression
 ///
 /// - SeeAlso: https://google.github.io/swift#error-types
-public final class NeverUseForceTry: SyntaxFormatRule {
+public final class NeverUseForceTry: SyntaxLintRule {
+  
+  // Checks if "XCTest" is an import statement
+  public override func visit(_ node: ImportDeclSyntax) {
+    var iterator = node.path.makeIterator()
+    while let component = iterator.next() {
+      if component.name.text == "XCTest" {
+        context.importsXCTest = true
+      }
+    }
+  }
+  
+  public override func visit(_ node: TryExprSyntax) {
+    if !context.importsXCTest {
+      guard let mark = node.questionOrExclamationMark else { return }
+      if mark.tokenKind == .exclamationMark {
+        diagnose(.doNotForceTry, on: node.tryKeyword)
+      }
+    }
+  }
+}
 
+extension Diagnostic.Message {
+  static let doNotForceTry = Diagnostic.Message(.warning, "Do not use force try")
 }
---
+++ tools/swift-format/Tests/SwiftFormatTests/NeverUseForceTryTests.swift
@@ -0,0 +1,21 @@
+import Foundation
+import XCTest
+import SwiftSyntax
+
+@testable import Rules
+
+public class NeverUseForceTryTests: DiagnosingTestCase {
+  public func testInvalidTryExpression() {
+    let input =
+      """
+      let document = try! Document(path: "important.data")
+      let document = try Document(path: "important.data")
+      let x = try! someThrowingFunction()
+      if let data = try? fetchDataFromDisk() { return data }
+      """
+    performLint(NeverUseForceTry.self, input: input)
+    XCTAssertDiagnosed(.doNotForceTry)
+    XCTAssertDiagnosed(.doNotForceTry)
+    XCTAssertNotDiagnosed(.doNotForceTry)
+  }
+}
