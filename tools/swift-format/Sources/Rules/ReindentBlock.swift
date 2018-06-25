bea0104a6658a4bf8ce5e1546abff3c3bd5810b4
Lauren White <laurenwhite@google.com>
2018-06-25 15:48:19 -0700

    Implement UseEarlyExits rule and tests.

---
+++ tools/swift-format/Sources/Rules/ReindentBlock.swift
@@ -0,0 +1,8 @@
+//
+//  ReindentBlock.swift
+//  Basic
+//
+//  Created by Lauren White on 6/21/18.
+//
+
+import Foundation
--- tools/swift-format/Sources/Rules/UseEarlyExits.swift
+++ tools/swift-format/Sources/Rules/UseEarlyExits.swift
@@ -15,5 +15,42 @@ import SwiftSyntax
 ///
 /// - SeeAlso: https://google.github.io/swift#guards-for-early-exits
 public final class UseEarlyExits: SyntaxFormatRule {
+  
+  public override func visit(_ node: CodeBlockSyntax) -> Syntax {
+    
+    var newItems: [CodeBlockItemSyntax] = []
+    for statement in node.statements {
+      guard let ifStmt = statement.item as? IfStmtSyntax else { return node }
+      guard let elseStmt = ifStmt.elseBody else { return node }
+      guard let elseBody = elseStmt as? CodeBlockSyntax else { return node }
 
+      if elseContainsControlStmt(elseStmt: elseStmt) {
+        guard let moveDeletedIfCode = visit(ifStmt.body.withLeftBrace(nil).withRightBrace(nil)) as? CodeBlockSyntax else { continue }
+        let formattedIfCode = reindentBlock(block: moveDeletedIfCode, adjustment: -2)
+        guard let moveElseBody = visit(elseBody) as? CodeBlockSyntax else { continue }
+        
+        let ifConditions = ifStmt.conditions
+        let formattedGuardKeyword = SyntaxFactory.makeGuardKeyword().withLeadingTrivia(ifStmt.ifKeyword.leadingTrivia).withOneTrailingSpace()
+        let newGuardStmt = SyntaxFactory.makeGuardStmt(guardKeyword: formattedGuardKeyword, conditions: ifConditions, elseKeyword: SyntaxFactory.makeElseKeyword().withOneTrailingSpace(), body: moveElseBody)
+        newItems.append(SyntaxFactory.makeCodeBlockItem(item: newGuardStmt, semicolon: nil, errorTokens: nil))
+        newItems.append(SyntaxFactory.makeCodeBlockItem(item: formattedIfCode, semicolon: nil, errorTokens: nil))
+      }
+    }
+    
+    let newNode = node.withStatements(SyntaxFactory.makeCodeBlockItemList(newItems))
+    return super.visit(newNode)
+  }
+
+  func elseContainsControlStmt(elseStmt: Syntax) -> Bool {
+    for child in elseStmt.children {
+      guard let codeBlockList = child as? CodeBlockItemListSyntax else { continue }
+      guard let last = codeBlockList.child(at: codeBlockList.count - 1) as? CodeBlockItemSyntax else { continue }
+      if last.item is ReturnStmtSyntax ||
+        last.item is ThrowStmtSyntax ||
+        last.item is BreakStmtSyntax {
+        return true
+      }
+    }
+    return false
+  }
 }
