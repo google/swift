240e08a6cf2e33a73add6854d99e3efa3b60b957
Lauren White <laurenwhite@google.com>
2018-06-27 13:31:17 -0700

    Implement NeverForceUnwrap rule and tests.

--- tools/swift-format/Sources/Rules/NeverForceUnwrap.swift
+++ tools/swift-format/Sources/Rules/NeverForceUnwrap.swift
@@ -8,5 +8,35 @@ import SwiftSyntax
 ///
 /// - SeeAlso: https://google.github.io/swift#force-unwrapping-and-force-casts
 public final class NeverForceUnwrap: SyntaxLintRule {
+  
+  // Checks if "XCTest" is an import statement
+  public override func visit(_ node: ImportDeclSyntax) {
+    var iterator = node.path.makeIterator()
+    while let component = iterator.next() {
+      if component.name.text == "XCTest" {
+        context.importsXCTest = true
+      }
+    }
+  }
+  
+  public override func visit(_ node: ForcedValueExprSyntax) {
+    guard !context.importsXCTest else { return }
+    print(node.expression.description)
+    diagnose(.doNotForceUnwrap(name: node.expression.description), on: node)
+  }
+  
+  public override func visit(_ node: AsExprSyntax) {
+    guard !context.importsXCTest else { return }
+    print(node.typeName.description)
+    diagnose(.doNotForceCast(name: node.typeName.description), on: node)
+  }
+}
 
+extension Diagnostic.Message {
+  static func doNotForceUnwrap(name: String) -> Diagnostic.Message {
+    return .init(.warning, "Do not force unwrap '\(name)'")
+  }
+  static func doNotForceCast(name: String) -> Diagnostic.Message {
+    return .init(.warning, "Do not force cast to '\(name)'")
+  }
 }
---
+++ tools/swift-format/Tests/SwiftFormatTests/DontRepeatTypeInStaticPropertiesTests.swift
@@ -0,0 +1,8 @@
+//
+//  DontRepeatTypeInStaticPropertiesTests.swift
+//  SwiftFormatTests
+//
+//  Created by Lauren White on 6/26/18.
+//
+
+import Foundation
