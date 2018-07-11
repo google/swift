bb1567e447d570247df9c730892f421b7573f161
Lauren White <laurenwhite@google.com>
2018-07-11 10:41:52 -0700

    Code review changes

--- tools/swift-format/Sources/Rules/ReturnVoidInsteadOfEmptyTuple.swift
+++ tools/swift-format/Sources/Rules/ReturnVoidInsteadOfEmptyTuple.swift
@@ -14,18 +14,18 @@ import SwiftSyntax
 /// - SeeAlso: https://google.github.io/swift#types-with-shorthand-names
 public final class ReturnVoidInsteadOfEmptyTuple: SyntaxFormatRule {
   public override func visit(_ node: FunctionTypeSyntax) -> TypeSyntax {
-    guard let returnType = node.returnType as? TupleTypeSyntax, returnType.elements.count == 0 else { return node }
+    guard let returnType = node.returnType as? TupleTypeSyntax,
+      returnType.elements.count == 0 else { return node }
     diagnose(.returnVoid, on: node.returnType)
-    var voidKeyword =  SyntaxFactory.makeSimpleTypeIdentifier(name: SyntaxFactory.makeIdentifier("Void"), genericArgumentClause: nil)
-    if let next = node.returnType.nextToken,
-      next.tokenKind != .rightParen,
-      !next.leadingTrivia.containsNewlines {
-      voidKeyword = voidKeyword.withName(voidKeyword.name.withOneTrailingSpace())
-    }
+    let voidKeyword =  SyntaxFactory.makeSimpleTypeIdentifier(
+      name: SyntaxFactory.makeIdentifier(
+        "Void",
+        trailingTrivia: returnType.rightParen.trailingTrivia),
+      genericArgumentClause: nil)
     return node.withReturnType(voidKeyword)
   }
 }
 
 extension Diagnostic.Message {
-  static let returnVoid = Diagnostic.Message(.warning, "Replace '()' with 'Void'")
+  static let returnVoid = Diagnostic.Message(.warning, "replace '()' with 'Void'")
 }
