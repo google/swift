45e66d44a35af1e6648130b16eb6ee128e108be1
Andrés Tamez <andrestamez@google.com>
2018-07-17 14:42:55 -0700

    Apply code review changes and add numberOfNewLines in Trivia+Convenience

--- tools/swift-format/Sources/Rules/CaseIndentLevelEqualsSwitch.swift
+++ tools/swift-format/Sources/Rules/CaseIndentLevelEqualsSwitch.swift
@@ -14,21 +14,25 @@ public final class CaseIndentLevelEqualsSwitch: SyntaxFormatRule {
   public override func visit(_ node: SwitchStmtSyntax) -> StmtSyntax {
     var cases = [Syntax]()
     guard let switchIndentation = node.leadingTrivia?.numberOfSpaces else { return node }
-    var numNewLines: Int
     var spacesDif: Int
     var isInvalid = false
-    
+
     // Iterates through the switch stamentent to ensure the number of spaces
     // in the indentation of each case is the same as the switch keyword.
     for caseExp in node.cases {
       guard let caseTrivia = caseExp.leadingTrivia else { continue }
-      
+
       if caseTrivia.numberOfSpaces != switchIndentation {
         spacesDif = switchIndentation - caseTrivia.numberOfSpaces
-        diagnose(.adjustIndentationSpaces(count: spacesDif, caseText: caseExp.description), on: node)
-        numNewLines = countNewLines(caseTrivia)
+        diagnose(
+          .adjustIndentationSpaces(
+          count: spacesDif,
+          caseText: caseExp.description),
+          on: node
+        )
+
         let newCase = replaceTrivia(on: caseExp, token: caseExp.firstToken, leadingTrivia:
-                Trivia.newlines(numNewLines).appending(.spaces(switchIndentation)))
+                Trivia.newlines(caseTrivia.numberOfNewlines).appending(.spaces(switchIndentation)))
         isInvalid = true
         cases.append(newCase)
       }
@@ -40,21 +44,13 @@ public final class CaseIndentLevelEqualsSwitch: SyntaxFormatRule {
   }
 }
 
-/// Returns the number of newlines in the given trivia.
-func countNewLines(_ trivia: Trivia) -> Int {
-  var count = 0
-  for piece in trivia {
-    if case .newlines(_) = piece {
-      count += 1
-    }
-  }
-  return count
-}
-
 extension Diagnostic.Message {
   static func adjustIndentationSpaces(count: Int, caseText: String) -> Diagnostic.Message {
     let adjustSpaces = count < 0 ? "remove" : "add"
     let ending = abs(count) == 1 ? "" : "s"
-    return Diagnostic.Message(.warning, "\(adjustSpaces) \(abs(count)) space\(ending) as indentation to the case \(caseText)")
+    return Diagnostic.Message(
+      .warning,
+      "\(adjustSpaces) \(abs(count)) space\(ending) as indentation to the case \(caseText)"
+    )
   }
 }
