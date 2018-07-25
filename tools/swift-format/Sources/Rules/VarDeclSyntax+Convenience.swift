707454ce971c778c59818c408ba2f67705033dc1
Lauren White <laurenwhite@google.com>
2018-07-25 13:33:03 -0700

    Added convenience

--- tools/swift-format/Sources/Rules/VarDeclSyntax+Convenience.swift
+++ tools/swift-format/Sources/Rules/VarDeclSyntax+Convenience.swift
@@ -2,6 +2,7 @@ import SwiftSyntax
 
 extension VariableDeclSyntax {
 
+  /// Returns array of all identifiers listed in the declaration.
   var identifiers: [IdentifierPatternSyntax] {
     var ids: [IdentifierPatternSyntax] = []
     for binding in bindings {
@@ -9,5 +10,28 @@ extension VariableDeclSyntax {
       ids.append(id)
     }
     return ids
+  }
+
+  /// Returns the first identifier.
+  var identifier: IdentifierPatternSyntax {
+    return identifiers[0]
+  }
+
+  /// Returns the type of the variable, if explicitly stated in the declaration.
+  var type: TypeSyntax? {
+    for binding in bindings {
+      guard let typeAnnotation = binding.typeAnnotation else { continue }
+      return typeAnnotation.type
+    }
+    return nil
+  }
+
+  /// Returns the initializer clause, if present.
+  var initializer: InitializerClauseSyntax? {
+    for binding in bindings {
+      guard let initializer = binding.initializer else { continue }
+      return initializer
+    }
+    return nil
   }
 }
